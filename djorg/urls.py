3461eb53c3f5ca067188cf847c9abec8d334599f
Cristina Laryea <crispad06@gmail.com>
2018-06-27 14:02:51 -0400

    added api, working, and posting on browser

--- djorg/urls.py
+++ djorg/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from notes.api import NoteViewset, PersonalNoteViewset
+
+routers = routers.DefaultRouter()
+routers.register(r'notes', NoteViewset)
+routers.register(r'personal_notes', PersonalNoteViewset)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'api/', include(routers.urls)),
 ]
